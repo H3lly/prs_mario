68f0763f337fd0eee94aa511e2822b4189fe465b
H3lly <h3lly.h0lly@gmail.com>
2017-01-04 18:36:49 +0100

    ajout du fichier maputils.c dans utils

--- util/maputils.c
+++ util/maputils.c
@@ -1,10 +1,14 @@
 
 /*
-get_width(file)
-get_height(file)
-get_objects(file) //nb objets contenus
-get_info(file) //affiche toutes les informations (facile...)
+get_width()
+get_height()
+get_objects() //nb objets contenus
+get_info() //affiche toutes les informations (facile...)
 
+set_width()
+set_height()
 
+set_objects(file, nb_frames, solidity, destructible, collectible, generator)
+prune_objects()
 
 */
